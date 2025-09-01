cec0a220e0835b3845b1b16c256174d3b7e4997b
Edomondo <edghimakoll@gmail.com>
2025-09-01 01:52:05 +0100

    chore(shared): some update on abstract methods

--- webapp/shared/infrastructure/repositories/__init__.py
+++ webapp/shared/infrastructure/repositories/__init__.py
@@ -1,27 +1,26 @@
 from abc import ABC, abstractmethod
-from typing import Any, Dict, Optional
 
 
 class BaseRepository(ABC):
     """Abstract base repository interface"""
 
     @abstractmethod
-    def get_by_id(self, entity_id: str):
+    def get_by_id(self, *args):
         """Get entity by ID"""
         pass
 
     @abstractmethod
-    def create(self, data: Dict[str, Any]):
+    def create(self, **kwargs):
         """Create new entity"""
         pass
 
     @abstractmethod
-    def update(self, entity, data: Dict[str, Any]):
+    def update(self, *args, **kwargs):
         """Update entity"""
         pass
 
     @abstractmethod
-    def delete(self, entity):
+    def delete(self, *args):
         """Delete entity"""
         pass
 
@@ -30,23 +29,23 @@ class UserRepositoryInterface(ABC):
     """Interface for user repository"""
 
     @abstractmethod
-    def get_by_username(self, username: str):
+    def get_by_username(self, *args):
         pass
 
     @abstractmethod
-    def get_by_email(self, email: str):
+    def get_by_email(self, *args):
         pass
 
     @abstractmethod
-    def exists_by_username(self, username: str) -> bool:
+    def exists_by_username(self, *args) -> bool:
         pass
 
     @abstractmethod
-    def exists_by_email(self, email: str) -> bool:
+    def exists_by_email(self, *args) -> bool:
         pass
-    
+
     @abstractmethod
-    def check_password(self, username: str, password: str) -> bool:
+    def check_password(self, *args) -> bool:
         pass
 
 
@@ -54,15 +53,7 @@ class ProjectRepositoryInterface(ABC):
     """Interface for project repository"""
 
     @abstractmethod
-    def get_by_owner(self, user):
-        pass
-
-    @abstractmethod
-    def get_with_task_statistics(self, user):
-        pass
-
-    @abstractmethod
-    def search(self, user, search_term: str):
+    def get_by_owner(self, *args, **kwargs):
         pass
 
 
@@ -70,19 +61,19 @@ class TaskRepositoryInterface(ABC):
     """Interface for task repository"""
 
     @abstractmethod
-    def get_by_project(self, project):
+    def get_by_project(self, **kwargs):
         pass
 
     @abstractmethod
-    def get_by_user(self, user):
+    def get_by_user(self, **kwargs):
         pass
 
     @abstractmethod
-    def search(self, user, search_term: str, project_id: Optional[str] = None):
+    def search(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def filter_by_status(self, user, status: str, project_id: Optional[str] = None):
+    def filter_by_status(self, *args, **kwargs):
         pass
 
 
@@ -90,17 +81,17 @@ class TimeEntryRepositoryInterface(ABC):
     """Interface for time entry repository"""
 
     @abstractmethod
-    def get_active_timer_for_user(self, user):
+    def get_active_timer_for_user(self, **kwargs):
         pass
 
     @abstractmethod
-    def get_active_timer_for_task(self, task, user=None):
+    def get_active_timer_for_task(self, **kwargs):
         pass
 
     @abstractmethod
-    def has_active_timer(self, task) -> bool:
+    def has_active_timer(self, **kwargs) -> bool:
         pass
 
     @abstractmethod
-    def stop_active_timers_for_user(self, user):
+    def stop_active_timers_for_user(self, **kwargs):
         pass
